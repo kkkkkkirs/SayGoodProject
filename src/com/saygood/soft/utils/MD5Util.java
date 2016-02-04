f5866b8e38a894c14bb809c73a3bacd89c762ef5
kaiqiangzhang <417079087@qq.com>
2016-02-04 23:51:32 +0800

    添加工具类

---
+++ src/com/saygood/soft/utils/MD5Util.java
@@ -0,0 +1,51 @@
+package com.saygood.soft.utils;
+
+import java.security.MessageDigest;
+/**
+ * Created by kriszhang on 16/2/4.
+ */
+public class MD5Util {
+    private final static String[] hexDigits = { "0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f" };
+
+    public static String byteArrayToHexString(byte[] b) {
+        StringBuffer resultSb = new StringBuffer();
+        for (int i = 0; i < b.length; i++) {
+            resultSb.append(byteToHexString(b[i]));
+        }
+        return resultSb.toString();
+    }
+
+    private static String byteToHexString(byte b) {
+        int n = b;
+        if (n < 0)
+            n = 256 + n;
+        int d1 = n / 16;
+        int d2 = n % 16;
+        return hexDigits[d1] + hexDigits[d2];
+    }
+
+    /**
+     * MD5Encode
+     * @param origin
+     * @return MD5 code
+     */
+    public static String MD5Encode(String origin) {
+        String resultString = null;
+
+        try {
+            resultString = new String(origin);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            resultString = byteArrayToHexString(md.digest(resultString
+                    .getBytes()));
+        } catch (Exception ex) {
+
+        }
+        return resultString;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(MD5Util.MD5Encode(String.valueOf(System.currentTimeMillis())));
+    }
+
+}
